9dba5577baf30cfd8094c304cd249c425401b861
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-05 09:12:59 -0700

    set item in text boxes to local storage

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
+  var save = $(".save-button");
+  var activityType = $(".form-control");
   console.log("ready!");
   function showTime() {
     var showTime = moment().format("MMMM Do YYYY, h:mm:ss a");
     $("#currentDay").html(showTime);
   }
   setInterval(showTime, 1000);
+  $(save).on("click", function () {
+    localStorage.setItem(
+      "activityOfTheDay: ",
+      JSON.stringify($(activityType).val())
+    );
+    // );
+    // var recordedActivity = localStorage.getItem("activityOfTheDay");
+    // save.html(recordedActivity);
+  });
 });
